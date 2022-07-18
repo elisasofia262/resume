694bd57971181ac3c3ea5af8ccc68d62ad379d13
elisasofia262 <elisasofia262@gmail.com>
2022-07-19 03:48:10 +0800

    Portfolio havent 2

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer id="footer">
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Elisa Sofia</span></strong>. All Rights Reserved
-      </div>
-    </div>
-</footer>
